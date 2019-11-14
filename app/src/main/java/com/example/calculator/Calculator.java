7d83f156ea3e597646b58d9e68aaecef1b652912
lane <lanefuerstenberg@gmail.com>
2019-11-14 09:30:17 -0600

    Didn't factor in both stagingArea and expression in the method

--- app/src/main/java/com/example/calculator/Calculator.java
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -92,6 +92,15 @@ public class Calculator {
             }
         }
 
+        for (char c : stagingArea.toCharArray()) {
+            if (c == '(') {
+                open++;
+
+            } else if (c == ')') {
+                closed++;
+            }
+        }
+
         return open > closed;
     }
 
